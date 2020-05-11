000d561c3ee9f8e515a46aebeea9439e5e88389f
Mario Merino <home@Marios-MBP.attlocal.net>
2020-05-11 15:09:26 -0500

    added user model and validation

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,29 @@
 const express = require('express');
+const User = require('..//models/User');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
 
 // @route   POST    api/users
 //@desc     register a new user
 //@access   Public
-router.post('/users', (req, res) => {
-  res.send('register a user');
-});
+router.post(
+  '/',
+  [
+    check('name', 'please a name is required').not().isEmpty(),
+    check('email', 'please include valid email').isEmail(),
+    check(
+      'password',
+      'please enter  password with 6 or more characters',
+    ).isLength({ min: 6 }),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    res.send('passed');
+  },
+);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require('express');
-
+const connectDB = require('./config/db');
 const app = express();
+
+//Connect DB
+connectDB();
+
+// Init mIddleware
+app.use(express.json({ extended: false }));
 
 app.get('/', (req, res) =>
   res.json({ msg: 'hello world this is the contact keeper API' }),
