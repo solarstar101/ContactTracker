187db53e46b5e98baa4cf77f2a8e14e9280f8098
Mario Merino <home@Marios-MBP.attlocal.net>
2020-05-11 16:26:52 -0500

    hashed passwords and register routes

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const User = require('..//models/User');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
 const { check, validationResult } = require('express-validator');
 
 // @route   POST    api/users
@@ -16,13 +17,39 @@ router.post(
       'please enter  password with 6 or more characters',
     ).isLength({ min: 6 }),
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
+
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
 
-    res.send('passed');
+    const { name, email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email });
+
+      if (user) {
+        return res.status(400).json({ msg: 'User already exist' });
+      }
+
+      user = new User({
+        name,
+        email,
+        password,
+      });
+
+      //salt to encrpyt password using method from bcrypty
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+
+      res.send('User saved');
+    } catch (err) {
+      console.error(error.message);
+      res.status(500).send('server error');
+    }
   },
 );
 
